af8593b205bff63d229d280849af9cfad8a1e9a2
Heng-Tze Cheng <hengtze.cheng@sv.cmu.edu>
2012-03-07 21:25:43 -0800

    Created charm.sensors package.

--- charm/src/edu/cmu/mobility/charm/CharmSettingsActivity.java
+++ charm/src/edu/cmu/mobility/charm/CharmSettingsActivity.java
@@ -1,13 +1,29 @@
 package edu.cmu.mobility.charm;
 
+import edu.cmu.mobility.charm.sensors.AccelerometerListener;
+import edu.cmu.mobility.charm.sensors.CompassListener;
 import android.app.Activity;
 import android.os.Bundle;
 
+
+
 public class CharmSettingsActivity extends Activity {
+	
+	private AccelerometerListener accelerometerListener;
+	private CompassListener compassListener;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.charm_settings_activity);
+        
+        initUserInterface();
+        
+        accelerometerListener = new AccelerometerListener(this);
+        compassListener = new CompassListener(this);
+    }
+    private void initUserInterface() {
+    	
     }
 }
